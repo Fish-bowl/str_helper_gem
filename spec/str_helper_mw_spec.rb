3bdf3368bba0a33c53d84ebfae99e8aa19fe20ce
Fish-bowl <symbolt.ap@gmail.com>
2018-10-15 10:56:52 -0600

    added specs

--- spec/str_helper_mw_spec.rb
+++ spec/str_helper_mw_spec.rb
@@ -1,9 +1,17 @@
 RSpec.describe StrHelperMw do
+  subject { StrHelperMw }
+
   it "has a version number" do
     expect(StrHelperMw::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do 
+    let(:str) {'My String'}
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirS yM')
+    end
   end
+
+  
 end
